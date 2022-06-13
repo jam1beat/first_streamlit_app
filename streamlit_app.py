14b5f3e1e940881ea8c24da07e7223687b5090e3
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-13 12:50:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title("May Parent New healthy Diner")
-streamlit.header('🥣Breakfest Manue')
-streamlit.text("Omega 2 Bluebery Oatmeal")
-streamlit.text("Kale and Spinch Roket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header('Breakfest Menue')
+streamlit.text('🥣 Omega 2 Bluebery Oatmeal')
+streamlit.text('🍶 Kale and Spinch Roket Smoothie')
+streamlit.text(' 🥚 Hard-Boiled Free-Range Egg')
