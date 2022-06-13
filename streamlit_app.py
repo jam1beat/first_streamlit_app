50559b79f102f822a7bdfaf6ad6442ff8b961aca
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-13 12:58:21 -0400

    Update streamlit_app.py
    Add DataFrame from S3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 
 import streamlit
+import pandas
 
 streamlit.title("May Parent New healthy Diner")
 streamlit.header('Breakfest Menue')
 streamlit.text('🥣 Omega 2 Bluebery Oatmeal')
 streamlit.text('🍶 Kale and Spinch Roket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
