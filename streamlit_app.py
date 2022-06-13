33e4a95e98dc570d0a68457844100ca1354233ef
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-13 13:12:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+
