53abca0281d5e2457b6bd72ad2fdd9fb71875427
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-13 13:03:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ streamlit.text('🍶 Kale and Spinch Roket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruite", list(my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
