8476d79062240526a7a094d0a7ebdea63f6790cb
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-18 15:12:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ streamlit.text('🍶 Kale and Spinch Roket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-fruity_normalizeed = pandas.json_normalize(frutiyvice_response.json())
+fruity_normalized = pandas.json_normalize(fruityvice_response.json())
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [1,16])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice")
-streamlit.dataframe(fruity_normalizeed)
+streamlit.dataframe(fruity_normalized)
