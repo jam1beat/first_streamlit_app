3b44f2c4e017d0cf3dbe14df66f2ee996f4edf67
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-20 12:31:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ streamlit.dataframe(my_data_rows)
 
 
 fruit_choice_b = streamlit.text_input('What fruit would you like to add? ', 'kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_choice_b)
