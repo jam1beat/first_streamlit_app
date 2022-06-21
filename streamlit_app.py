be2e51f4a7732ebd83d3d7ae39f52d1a0dd6faa7
Jam1beat <107423272+jam1beat@users.noreply.github.com>
2022-06-21 12:33:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,11 @@ def get_fruity_vice_data(this_fruite_choice):
     fruity_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruity_normalized
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute ("select * from fruit_load_list")
+         retrun my_cur.fetchall()
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [1,16])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -34,10 +39,9 @@ try:
 except URLError as e:
   streamlit.stop()
 
+if streamlit.button('Get Fruit Load List')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from FRUIT_LOAD_LIST;")
-my_data_rows = my_cur.fetchall()
+my_data_rows =  get_fruit_load_list()
 streamlit.header("The FRUIT_LOAD_LIST contains:")
 streamlit.dataframe(my_data_rows)
 
